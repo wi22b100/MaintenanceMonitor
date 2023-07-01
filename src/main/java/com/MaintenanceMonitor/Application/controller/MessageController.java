901c5d9b8cffe1d1ef60cff32a45df88b5f6c0ca
Halil TUNCER <wi22b100@technikum-wien.at>
2023-07-01 11:43:48 +0200

    Changed package names, implemented the MessageService and MessageController classes. The first Feature is implemented which is showing the Defaultmessage on the browser.

---
+++ src/main/java/com/MaintenanceMonitor/Application/controller/MessageController.java
@@ -0,0 +1,24 @@
+package com.MaintenanceMonitor.Application.controller;
+
+import com.MaintenanceMonitor.Application.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class MessageController {
+
+    private final MessageService messageService;
+
+    @Autowired
+    public MessageController(MessageService messageService) {
+        this.messageService = messageService;
+    }
+
+    @GetMapping("/message")
+    public String getDefaultMessage() {
+        return messageService.getDefaultMessage();
+    }
+}
